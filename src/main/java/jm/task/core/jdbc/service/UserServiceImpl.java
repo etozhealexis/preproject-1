3cc51653b88eb5dcb09e44d8fb01b9480171b9aa
etozhealexis <alexisworksya@yandex.ru>
2023-03-21 01:16:23 +0300

    dao

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,36 +8,31 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    private UserDao dao;
     public UserServiceImpl() {
-
+        this.dao = new UserDaoHibernateImpl();
     }
     public void createUsersTable() {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        UserDao dao = new UserDaoHibernateImpl();
         return dao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        UserDao dao = new UserDaoHibernateImpl();
         dao.cleanUsersTable();
     }
 }
